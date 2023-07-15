aebdd9b9c34c6f5f30456a8993f00df1ece4d04e
Felix Bausemer <felix.bausemer@pohlcon.com>
2023-07-15 23:29:38 +0200

    [ADD] routes added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Golang-BooksAPI
 A simple books api to learn the basics of Golang
+
+Current video: https://www.youtube.com/watch?v=bj77B59nkTQ&t=1740s
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "errors"
 	"github.com/TwiN/go-color"
 	"fmt"
 	"net/http"
@@ -9,23 +8,42 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type book struct{
-	ID string 		`json:"id"`
-	Title string 	`json:"title"`
-	Author string `json:"author"`
-	Quantity int 	`json:"quantity"`
-}
-
-var books = []book{
-	{ID: "1", Title: "Harry Potter and the Philosopher's Stone", Author: "J.K.Rowling", Quantity: 2},
-	{ID: "2", Title: "Harry Potter and the Chamber of Secrets", Author: "J.K.Rowling", Quantity: 3},
-	{ID: "3", Title: "Harry Potter and the Prisoner of Azkaban", Author: "J.K.Rowling", Quantity: 3},
-	{ID: "4", Title: "Harry Potter and the Goblet of Fire", Author: "J.K.Rowling", Quantity: 2},
-	{ID: "5", Title: "Harry Potter and the Order of the Phoenix", Author: "J.K.Rowling", Quantity: 0},
-}
 
 func getBooks(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, books)
+}
+
+func createBook(context *gin.Context){
+	var newBook book
+	
+	if err := context.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	highestId := -1
+	for _, book := range books {
+		if book.ID > highestId {
+			highestId = book.ID + 1
+		}
+	}
+
+	newBook.ID = highestId
+
+	books = append(books, newBook)
+	context.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func getBook(context *gin.Context){
+	id := context.Param("id")
+	idAsInt, _ := strconv.Atoi(id)
+	book, err := getBookById(idAsInt)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return 
+	}
+
+	context.IndentedJSON(http.StatusOK, book)
 }
 
 func main() {
@@ -33,5 +51,7 @@ func main() {
 	fmt.Println(color.Ize(color.Green,"Api is starting on port: "+strconv.Itoa(port)))
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.GET("/books/:id", getBook)
+	router.POST("/books", createBook)
 	router.Run("localhost:"+strconv.Itoa(port))
 }
